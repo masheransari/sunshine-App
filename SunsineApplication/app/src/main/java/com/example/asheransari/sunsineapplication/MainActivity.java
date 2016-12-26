0eb4649431f75ead91f8e7f576fd8afa21f16bd2
Muhammad Asher Ansari <asher.ansari@nhu.edu.pk>
2016-12-26 18:06:37 +0500

    now i am created a temp sunshine app

--- SunsineApplication/app/src/main/java/com/example/asheransari/sunsineapplication/MainActivity.java
+++ SunsineApplication/app/src/main/java/com/example/asheransari/sunsineapplication/MainActivity.java
@@ -2,14 +2,21 @@ package com.example.asheransari.sunsineapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView t1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forcast);
+
+        t1 = (TextView)findViewById(R.id.textView);
+//        Log.
+
     }
 }
